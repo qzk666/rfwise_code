b5ef84c46544cd3cdb50c093ed2bafcd020e460f
qzk <1304216352@qq.com>
2023-01-13 17:17:34 +0800

    ddd

--- variance.m
+++ variance.m
@@ -1,8 +1,8 @@
 function res = variance(data)
 n = 10;
 res = [];
-for i =1:length(data)-9
-    temp = data(i:i+9);
+for i =1:length(data)-(n-1)
+    temp = data(i:i+(n-1));
     va = std(temp,1);
     res = [res,va];
 end
